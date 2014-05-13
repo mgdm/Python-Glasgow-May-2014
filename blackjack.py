63fbcb18d8280fcc89db0225e3530bb451ef530c
Michael Maclean <michael@no-surprises.co.uk>
2014-05-13 21:01:06 +0100

    Stuff

--- blackjack.py
+++ blackjack.py
@@ -1,18 +1,66 @@
 from random import shuffle
 
+def hand_value(hand):
+    r = 0
+    for c in hand:
+        r += c[1]
+
+    return r
+
 class Deck:
 
     suits = ['hearts', 'diamonds', 'clubs', 'spades']
-    values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
-    cards = []
+    values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-    def __init__(self):
+    def __init__(self, shuffle_func=None):
+        self.cards = []
+
+        if shuffle_func is None:
+            self.shuffle_func = shuffle
+        else:
+            self.shuffle_func = shuffle_func
+
         for s in self.suits:
             for v in self.values:
                 self.cards.append((s, v))
 
-    def shuffle(self, shuffle_func):
-        shuffle_func(self.cards)
+    def shuffle(self):
+        self.shuffle_func(self.cards)
         
     def deal(self):
-        return self.cards.pop()
+        return self.cards.pop(0)
+
+class Game:
+
+    def __init__(self, player_hands):
+        self.deck = Deck()
+        self.deck.shuffle()
+        self.dealer_hand = []
+
+        self.player_hands = []
+        for i in range(0, player_hands):
+            self.player_hands.append([])
+
+    def initial_deal(self):
+        self._deal_players()
+        self.dealer_hand.append(self.deck.deal())
+        self._deal_players()
+
+    def play(self):
+        pass
+
+    def _deal_players(self):
+        for hand in self.player_hands:
+            hand.append(self.deck.deal())
+
+
+if __name__ == '__main__':
+    players = 4
+
+    game = Game(players)
+    game.initial_deal()
+
+    for i in range(0, players):
+        print "Player %d has %d" % (i + 1, hand_value(game.player_hands[i]))
+
+    print "Dealer has %d" % hand_value(game.dealer_hand)
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import unittest
-
 import copy
 
-from blackjack import Deck
+from blackjack import Deck, Game, hand_value
 
 def shuffle_func(list):
     first_card = list[0]
@@ -13,23 +12,68 @@ def shuffle_func(list):
 class TestDeck(unittest.TestCase):
 
     def setUp(self):
-        self.deck = Deck()
+        self.deck = Deck(shuffle_func)
 
     def test_cards(self):
         self.assertEqual(52, len(self.deck.cards))
 
     def test_shuffle(self):
         cards_before = list(self.deck.cards)
-        self.deck.shuffle(shuffle_func)
+        self.deck.shuffle()
         cards_after = list(self.deck.cards)
 
         self.assertNotEqual(cards_before, cards_after)
 
     def test_deal(self):
-        expected = self.deck.cards[51]
+        expected = self.deck.cards[0]
         card = self.deck.deal()
 
         self.assertEqual(expected, card)
+
+    def test_deal_card_gone(self):
+        card = self.deck.deal()
+        self.assertEqual(51, len(self.deck.cards))
+
+
+class TestGame(unittest.TestCase):
+
+    def setUp(self):
+        self.player_hands = 4
+        self.game = Game(self.player_hands)
+   
+    def test_deck(self):
+        self.assertEqual(self.game.deck.__class__, Deck)
+
+    def test_player_hands(self):
+        self.game.initial_deal()
+        self.assertEqual(self.player_hands, len(self.game.player_hands))
+        for hand in self.game.player_hands:
+            self.assertEqual(2, len(hand))
+
+    def test_dealer_hand(self):
+        self.game.initial_deal()
+        self.assertEqual(1, len(self.game.dealer_hand))
+
+    def test_initial_deal(self):
+        dealt_cards = self.game.deck.cards[0:9]
+        
+        self.game.initial_deal()
+
+        self.assertEqual(dealt_cards[0], self.game.player_hands[0][0])
+        self.assertEqual(dealt_cards[1], self.game.player_hands[1][0])
+        self.assertEqual(dealt_cards[2], self.game.player_hands[2][0])
+        self.assertEqual(dealt_cards[3], self.game.player_hands[3][0])
+        self.assertEqual(dealt_cards[4], self.game.dealer_hand[0])
+        self.assertEqual(dealt_cards[5], self.game.player_hands[0][1])
+        self.assertEqual(dealt_cards[6], self.game.player_hands[1][1])
+        self.assertEqual(dealt_cards[7], self.game.player_hands[2][1])
+        self.assertEqual(dealt_cards[8], self.game.player_hands[3][1])
+
+
+class TestHandValue(unittest.TestCase):
+    def test_value_of_hand(self):
+        result = hand_value([('hearts', 1), ('diamonds', 4), ('clubs', 10), ('clubs', 6)])
+        self.assertEqual(21, result)
 
 if __name__ == '__main__':
     unittest.main() 
